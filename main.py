4953c7ac34f8597dfa0c5ff761124dee63e4cb64
En7ity303 <81084296+En7ity303@users.noreply.github.com>
2024-08-03 22:04:27 +0200

    Update main.py
    Big improvements in readability of the code and improved commands handling

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-from pytubefix import YouTube
-from pytubefix import Playlist
+from pytubefix import YouTube, Playlist
 import os
 from sys import platform
 from pydub import *
-from src import art
-from src import downloader
-from src import txtcolors
-from src import file
+from src import art, downloader, txtcolors, cmd
 
 #This sofwtare is for EDUCATIONAl PURSPOSE ONLY, the author do not take any responsibility for your use, downloading youtube content is againts the copyright and against youtube terms
 
-      
 
-
-def disc():
+def disclaimer():
     print(txtcolors.warn("\n\nDownloading YouTube video is against copyright and YouTube terms, do you wanna continue? If you go ahead the author of this tool does not take any responsibility for your usage"))
     resp=input(">>>")
     if resp!="YES" and resp!="Yes" and resp!="yes":
         quit()
         
-def clear():
-    if platform=="linux" or platform=="linux2" or platform=="darwin":
-            os.system("clear")
-    elif platform=="win32":
-            os.system("cls")
-            
-    print(txtcolors.warn(art.art))
-
 
 def pl():
     p=Playlist(input("Enter the YouTube playlist URL: \n>>>"))
@@ -47,6 +33,7 @@ def pl():
             video=YouTube(url)
             downloader.Downloadmp4(video)
 
+
 def video():
     link=input("Enter the YouTube video URL: \n>>>")
     video=YouTube(link)
@@ -62,68 +49,38 @@ def video():
     if ext=="mp4" or ext=="2":
         downloader.Downloadmp4(video)
 
+
+
 ##  MAIN  ##
-disc()
+disclaimer()
 flag_pl=False
 
 while True:
-    while True:
-        txtcolors.clear()
-        ans=input("Press 'm' to modify the path where the files are downloaded\nPress 'r' to modify the default resolution\nPress 'p' to show the current file download path\nPress 'x' to reset all the settings\nPress 's' to start the program\nPress 'e' to close the program\n\n>>>")
-
-        if ans=='m':
-            txtcolors.clear()
-            file.new_path()
-        
-        elif ans=='p':
-            txtcolors.clear()
-            print("The actual path is:\n\n\n>\t"+file.show_path()+"\t\t<")
-            input("\n\nPress enter to go ahead\n>>>")
-        
-        elif ans=='r':
-            txtcolors.clear()
-            file.new_res()
-        
-        elif ans=='x':
-            while True:
-                txtcolors.clear()
-                res=input("Do you really want to restore all settings?\n\n>>>")
-                if res=='1' or res=="yes" or res=="Yes" or res=="YES" or res=="y":
-                    file.panic()
-                    break
-                break
-        
-        elif ans=='s':
-            break
-            
-        elif ans=='e':
-            txtcolors.totcls()
-            input(txtcolors.warn("Have a nice day!"))
-            txtcolors.totcls()
-            quit()
-
-
-    x="."
-    while x!="video" and x!="playlist" and x!="Video" and x!= "Playlist" and x!="pl" and x!="vd" and x!="1" and x!="2":
-        txtcolors.clear()
-        x=str(input("Choose what you wanna download:\n>Video<\n>Playlist<\n\n>>>"))
-
     txtcolors.clear()
-    while True:
-        if x=="video" or x=="Video" or x=="vd" or x=="1":
-            video()
-            ans=input("\nDo you wanna download another video?\n>>>")
-            if ans=='nope' or ans=='no' or ans=='No' or ans=='Nope' or ans=='nop':
-                break
-            txtcolors.clear()
     
-        if x=="Playlist" or x=="playlist" or x=="pl" or x=="2":
-            pl()
-            flag_pl=True
+    cmd.get_command()
 
-        if flag_pl==True:
+        
+    recursive=True
+    while recursive:
+        x="."
+        while x!="video" and x!="playlist" and x!="Video" and x!= "Playlist" and x!="pl" and x!="vd" and x!="1" and x!="2":
             txtcolors.clear()
-            downloader.toterr()
+            x=str(input("Choose what you wanna download:\n>Video<\n>Playlist<\n\n>>>"))
 
-        break
-    txtcolors.clear()
+        txtcolors.clear()
+        while True:
+            if x=="video" or x=="Video" or x=="vd" or x=="1":
+                video()
+                ans=input("\nDo you wanna download another video?\n>>>")
+                if ans=='nope' or ans=='no' or ans=='No' or ans=='Nope' or ans=='nop':
+                    recursive==False
+                txtcolors.clear()
+    
+            if x=="Playlist" or x=="playlist" or x=="pl" or x=="2":
+                pl()
+                txtcolors.clear()
+                downloader.toterr()
+
+            break
+        txtcolors.clear()
